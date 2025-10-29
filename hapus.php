c385776c57df9ee635f16419f45af271cea73575
kharizma <h1101241050@student.untan.ac.id>
2025-10-29 14:36:18 +0700

    Initial commit

---
+++ hapus.php
@@ -0,0 +1,14 @@
+<?php
+require_once "config/Database.php";
+require_once "classes/Mahasiswa.php";
+
+$database = new Database();
+$db = $database->getConnection();
+
+$mhs = new Mahasiswa($db);
+$mhs->id = $_GET['id'];
+
+if ($mhs->delete()) {
+    header("Location: index.php");
+}
+?>
