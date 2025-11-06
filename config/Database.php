c0c33b39e4a7d0eb53655f183a40ad8247c40c2c
Naimakhaa <h1101241050@student.untan.ac.id>
2025-11-06 17:12:10 +0700

    Update Database.php

--- config/Database.php
+++ config/Database.php
@@ -1,14 +1,20 @@
 <?php
-$host = 'localhost';
-$dbname = 'nama_database_kamu';
-$username = 'root';
-$password = '';
+class Database {
+    private $host = "localhost";
+    private $db_name = "kampus";
+    private $username = "root";
+    private $password = "";
+    public $conn;
 
-try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Koneksi berhasil!";
-} catch (PDOException $e) {
-    echo "Koneksi gagal: " . $e->getMessage();
+    public function getConnection() {
+        $this->conn = null;
+        try {
+            $this->conn = new PDO("mysql:host={$this->host};dbname={$this->db_name}", $this->username, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $e) {
+            echo "Koneksi gagal: " . $e->getMessage();
+        }
+        return $this->conn;
+    }
 }
 ?>
